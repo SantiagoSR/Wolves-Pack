1a2b528e443c68ec37a48773118ebfae43276684
SantiagoSR <46933085+SantiagoSR@users.noreply.github.com>
2021-09-25 09:23:00 -0500

    Update Client-Peer.py

--- Client-Peer.py
+++ Client-Peer.py
@@ -1,16 +1,25 @@
 import requests
+import urllib.request
 import cmd
+import os
 
 
-def FILE(url):
+def FILE(data):
     #global STATUS_ADDR
     #STATUS_ADDR = "http://ec2-3-85-36-56.compute-1.amazonaws.com:3000"
     #r = requests.post('http://ec2-3-85-36-56.compute-1.amazonaws.com:3000', data = {'url': url})
     #print(f"{r}")
-    r = requests.post('http://ec2-52-91-144-8.compute-1.amazonaws.com:3000', data = {'url': url})
+    r = requests.post('http://ec2-52-91-144-8.compute-1.amazonaws.com:3000', data = {'url': data})
     print(f"{r.text}")
 def UPLOAD(data):
-    r = requests.post('http://ec2-52-91-144-8.compute-1.amazonaws.com:3000', data = {'upload': data})
+    if os.getcwd()[-4] != 'data': 
+        os.chdir(os.getcwd()+'/data')
+    external_ip =str(urllib.request.urlopen('https://ident.me').read().decode('utf8'))
+    dataxd = data #os.listdir()
+    print(dataxd)
+    print(external_ip)
+    #data = 'datos.txt'
+    r = requests.post('http://ec2-52-91-144-8.compute-1.amazonaws.com:3000', data = {'upload': dataxd, 'ip' : external_ip})
     print(f"{r.text}")
 
 class ShortyShell(cmd.Cmd):
@@ -21,9 +30,9 @@ class ShortyShell(cmd.Cmd):
         'Generates FILE based on URL: FILE <URL> '
         FILE(str(arg))
     
-    def do_UPLOAD(self, arg):
+    def do_UPLOAD(self, args):
         'UPLOAD DATA FROM FOLDER'
-        UPLOAD(str(arg))
+        UPLOAD(str(args))
 
 def main():
     shell = ShortyShell()
